c0ab6698233b82171d452e48557d4b2b959d8c9f
cristina-grosu <cristina.grosu@bigstep.com>
2019-05-06 10:36:31 +0300

    Create execute-notebook.sh
    Added a notebook that creates the demo files and tables in the project.

---
+++ execute-notebook.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+counter="0"
+
+#MODEL_BDL_FILE=`basename $MODEL_BDL_PATH`
+
+while [ $counter -lt 10 ]
+do
+echo "Trying to execute local notebook" 
+jupyter nbconvert --to notebook --execute /lentiq/notebooks/on-boarding.ipynb
+rm /lentiq/notebooks/on-boarding.ipynb
+
+FILE=/lentiq/notebooks/HairEyeColor.csv
+if test -f "$FILE"; then
+    echo "$FILE exists"
+    rm /lentiq/notebooks/HairEyeColor.csv
+    exit 0
+fi
+else 
+  echo "$FILE does not exist"
+  sleep 10
+  counter=$[$counter+1]
+fi
+done
+
+#curl -XPUT -H "content-type: application/json"   -d "{\"path\":\"/models/$MODEL_BDL_FILE\"}" http://localhost:65327/model
+#result=$?
+#if [ $result -eq 0 ]; then
+#	echo "Succesfully loaded the model" >> /tmp/model_load
+#	exit 0
+#else
+#	echo "Failed to load the model" >> /tmp/model_load
+#	sleep 10
+#	counter=$[$counter+1]
+#fi
+#done
+
+echo "Failed to find the file,killing main process" 
+# if it wasn't able to perform the load after 10 tries, kill the main process
+kill 1
