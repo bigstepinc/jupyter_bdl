bee540613492583f52f61e45f369939664e13d02
cristina-grosu <cristina.grosu@bigstep.com>
2019-05-15 18:12:36 +0300

    Update execute-notebook.sh

--- execute-notebook.sh
+++ execute-notebook.sh
@@ -2,25 +2,27 @@
 
 counter="0"
 
+echo "Trying to download the script from the repo"
+cd /opt && wget https://repo.lentiq.com/starter.py 
+
 while [ $counter -lt 10 ]
 do
 echo "Trying to execute local notebook" 
-jupyter nbconvert --to notebook --execute /lentiq/notebooks/on-boarding.ipynb
-rm /lentiq/notebooks/on-boarding.ipynb
+python /opt/starter.py >> /opt/output.txt
 
-FILE=/lentiq/notebooks/HairEyeColor.csv
-if test -f "$FILE"; then
-    echo "$FILE exists"
-    rm /lentiq/notebooks/HairEyeColor.csv
-    rm /lentiq/notebooks/on-boarding.nbconvert.ipynb
+FILE=/opt/output.txt
+if [[ 'grep 'DONE' $FILE' ]];then
+    echo "The execution of the starter script has ended successfully"
+    rm /opt/output.txt
+    rm /opt/starter.py
     exit 0
 else 
-  echo "$FILE does not exist"
+  echo "The execution of the starter script has not ended successfully"
   sleep 10
   counter=$[$counter+1]
 fi
 done
 
-echo "Failed to find the file,killing main process" 
+echo "Failed to execute the starter script, killing main process" 
 # if it wasn't able to perform the load after 10 tries, kill the main process
 kill 1
